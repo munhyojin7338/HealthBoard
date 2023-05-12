4d9f39256ff818b269acf9399680fd2ae5dc4e2a
MUN-HyoJin <hyojin7338@naver.com>
2023-05-12 23:40:52 +0900

    HealthBoard 초기

---
+++ src/main/java/com/example/healthboard/HealthBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.healthboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/healthboard/HealthBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.healthboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
